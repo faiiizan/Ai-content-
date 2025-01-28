0e4e3325361b94374b6a5dc6ba0cc1a764689cda
raja-desuntechnology <raja.desuntechnology@gmail.com>
2025-01-28 20:09:41 +0530

    add blog page

--- .next/static/chunks/pages/dashboard.js
+++ .next/static/chunks/pages/dashboard.js
@@ -1,12 +1,8 @@
 __turbopack_load_page_chunks__("/dashboard", [
-  "static/chunks/node_modules_next_dist_b8496e._.js",
+  "static/chunks/node_modules_next_dist_f1b02b._.js",
   "static/chunks/node_modules_react-dom_82bb97._.js",
-  "static/chunks/node_modules_@mui_system_esm_a01d62._.js",
-  "static/chunks/node_modules_@mui_material_362d64._.js",
-  "static/chunks/node_modules_@toolpad_core_b931f9._.js",
-  "static/chunks/node_modules_@popperjs_core_lib_515829._.js",
-  "static/chunks/node_modules_5649b8._.js",
+  "static/chunks/node_modules_beb007._.js",
   "static/chunks/[root of the server]__0bd49b._.js",
   "static/chunks/src_pages_dashboard_5771e1._.js",
-  "static/chunks/src_pages_dashboard_e3fef6._.js"
+  "static/chunks/src_pages_dashboard_39ef2c._.js"
 ])
