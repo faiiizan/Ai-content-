242d60e5ddacb99a803c96162fa903e81ffc92bb
raja-desuntechnology <raja.desuntechnology@gmail.com>
2025-02-03 18:53:05 +0530

    changes in image generator page

--- src/pages/dashboard/image/index.tsx
+++ src/pages/dashboard/image/index.tsx
@@ -26,41 +26,12 @@ import { PiChartPieSliceFill } from "react-icons/pi";
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: number;
-    value: number;
-}
 
-function CustomTabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-        </div>
-    );
-}
 
-function a11yProps(index: number) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
-}
 
 const Imagegenertor = () => {
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-    };
+   
 
 
     const [open, setOpen] = useState(false); // State to control drawer visibility
